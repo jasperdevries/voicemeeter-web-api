56db829e3f413c61b885f0b35b51fbf3da4a87bf
Jasper de Vries <jasper@thevries.dev>
2022-02-23 15:40:52 +0100

    add endpoint to get the sound files within a directory

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 require('dotenv').config();
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = process.env.API_PORT;
 
+const soundExtensions = ['.wav', '.mp3'];
+
 app.get('/', (req, res) => {
     res.contentType('application/json');
     res.send({
         success: true,
+    });
+});
+
+app.get('/get-sounds', (req, res) => {
+    const directory = process.env.SOUND_DIRECTORY;
+    const files = fs.readdirSync(directory)
+        .map(item => {
+            if (soundExtensions.includes(path.extname(item))) {
+                return item;
+            }
+        }).filter(item => item !== null && typeof item !== "undefined");
+
+    res.contentType('application/json');
+    res.send({
+        success: true,
+        files: files,
     });
 });
 
